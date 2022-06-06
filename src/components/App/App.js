fdaa80645d3b5781e2a44bba7be0d2be06c2129a
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2022-06-06 22:34:47 +0200

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,29 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import data from '../../data.json';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPlanet: data[0],
-    };
-  }
+const App = () => {
+  const [currentPlanet, setCurrentPlanet] = useState(data[0]);
 
   // update the state for contain the data with the good index which was send by the header component
-  handleChangePlanets = (index) => {
-    if (this.state.currentPlanet !== data[index]) {
-      this.setState({
-        currentPlanet: data[index],
-      });
+  const handleChangePlanets = (index) => {
+    if (currentPlanet !== data[index]) {
+      setCurrentPlanet(data[index]);
     } else {
       return;
     }
   };
 
-  render() {
-    const currentPlanet = this.state.currentPlanet;
-    return (
-      <>
-        <Header dataJson={data} handleChangePlanets={this.handleChangePlanets} currentPlanet={currentPlanet} />
-        <Main dataJson={currentPlanet} />
-        <Footer dataJson={currentPlanet} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Header dataJson={data} handleChangePlanets={handleChangePlanets} currentPlanet={currentPlanet} />
+      <Main dataJson={currentPlanet} />
+      <Footer dataJson={currentPlanet} />
+    </>
+  );
+};
 
 export default App;
